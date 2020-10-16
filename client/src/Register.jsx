59127d7a3aa6b6a0c9b3df087b5a6ee8eab30778
Hewah Bahrami <hewbahrami@gmail.com>
2020-10-16 16:38:46 -0400

    makes post request to server for registering

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
   const [userState, setUserState] = useState({
@@ -6,16 +7,43 @@ const Register = () => {
     lastName: '',
     email: '',
     pass: '',
-    passVerification: ''
+    passConfirm: ''
   });
   const [redirect, setRedirect] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserState(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const { firstName, lastName, email, pass, passConfirm } = userState;
+    if (!email.includes('@')) {
+      window.alert("Please enter a valid email address")
+    } else if (pass !== passConfirm) {
+      window.alert("Password incorrect. Please re-enter a valid password")
+    } else {
+      axios.post('/register', { firstName, lastName, email, pass })
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
 
   return (
     <div>
       <div className="jumbotron">Register</div>
       <div>
-        <form>
+        <form onSubmit={submitForm}>
           <input
+            onChange={handleInputChange}
             type="text"
             name="firstName"
             placeholder="First Name"
@@ -23,6 +51,7 @@ const Register = () => {
             required
           ></input>
           <input
+            onChange={handleInputChange}
             type="text"
             name="lastName"
             placeholder="Last Name"
@@ -30,6 +59,7 @@ const Register = () => {
             required
           ></input>
           <input
+            onChange={handleInputChange}
             type="text"
             name="email"
             placeholder="Email"
@@ -37,6 +67,7 @@ const Register = () => {
             required
           ></input>
           <input
+            onChange={handleInputChange}
             type="text"
             name="pass"
             placeholder="Password"
@@ -44,20 +75,17 @@ const Register = () => {
             required
           ></input>
           <input
+            onChange={handleInputChange}
             type="text"
-            name="pass"
-            placeholder="Password"
-            className="form-control my-4"
-            required
-          ></input>
-          <input
-            type="text"
-            name="passVerification"
+            name="passConfirm"
             placeholder="Re-enter Password"
             className="form-control my-4"
             required
           ></input>
-          <button type="button" className="btn btn-block btn-secondary">Register</button>
+          <button
+            onClick={submitForm}
+            type="button"
+            className="btn btn-block btn-secondary">Register</button>
         </form>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ const app = express();
 const path = require('path');
 const passport = require('passport');
 const db = require('../database/index.js');
+const bodyParser = require('body-parser');
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json())
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/register', express.static(path.join(__dirname, '../public')));
 app.use('/login', express.static(path.join(__dirname, '../public')));
 
+app.post('/register', (req, res) => {
+  res.send('ok!')
+})
 
 const port = 1234;
 
